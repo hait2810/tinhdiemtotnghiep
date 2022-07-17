95ee04f278719a1236b40a10c8de14e29068c9bc
Hai <haibong2001@gmail.com>
2022-07-18 06:46:20 +0700

    hj

--- main.js
+++ main.js
@@ -32,20 +32,23 @@ const btnthpt = document.querySelector(".btn_thpt");
 btnthpt.addEventListener("click", (e) => {
   e.preventDefault();
 
-  const toan = parseInt(document.querySelector(".toan").value);
-  const nguvan = parseInt(document.querySelector(".nguvan").value);
-  const ngoaingu = parseInt(document.querySelector(".ngoaingu").value);
-  const lichsu = parseInt(document.querySelector(".lichsu").value);
-  const hoa_dia = parseInt(document.querySelector(".hoa_dia").value);
-  const sinh_gdcd = parseInt(document.querySelector(".sinh_gdcd").value);
-  const tb12 = parseInt(document.querySelector(".tb12").value);
-  const khuyenkhich = parseInt(document.querySelector(".khuyenkhich").value);
-  const uutien = parseInt(document.querySelector(".uutien").value);
+  const toan = parseFloat(document.querySelector(".toan").value);
+  const nguvan = parseFloat(document.querySelector(".nguvan").value);
+  const ngoaingu = parseFloat(document.querySelector(".ngoaingu").value);
+  const lichsu = parseFloat(document.querySelector(".lichsu").value);
+  const hoa_dia = parseFloat(document.querySelector(".hoa_dia").value);
+  const sinh_gdcd = parseFloat(document.querySelector(".sinh_gdcd").value);
+  const tb12 = parseFloat(document.querySelector(".tb12").value)
+  const khuyenkhich = parseFloat(document.querySelector(".khuyenkhich").value);
+  const uutien = parseFloat(document.querySelector(".uutien").value);
   const show = document.querySelector(".show_point");
   const noti = document.querySelector(".noti");
   const monphu = (lichsu + hoa_dia + sinh_gdcd) / 3;
   const diem4baithi = (toan + nguvan + ngoaingu + monphu + khuyenkhich) / 4;
-  const sum = ((diem4baithi * 7 + (tb12*3)) / 10) + uutien ;
+  const sum1 = (diem4baithi*7+(tb12*3))/10
+  const sum = sum1 + uutien;
+  console.log(uutien);
+  console.log(tb12);
   show.classList.add('btn-info')
   show.innerHTML = sum
   
@@ -64,12 +67,12 @@ if(nguvan <= 1 || ngoaingu <= 1 || lichsu <= 1 || hoa_dia <= 1 || sinh_gdcd <= 1
 const btngdtx = document.querySelector(".btn_gdtx");
 btngdtx.addEventListener("click", (e) => {
     e.preventDefault()
-const toan = parseInt(document.querySelector(".toana").value);
-  const nguvan = parseInt(document.querySelector(".nguvana").value);
-  const tohop = parseInt(document.querySelector(".tohopa").value);
-  const tb12 = parseInt(document.querySelector(".tb12a").value);
-  const khuyenkhich = parseInt(document.querySelector(".khuyenkhicha").value);
-  const uutien = parseInt(document.querySelector(".uutiena").value);
+const toan = parseFloat(document.querySelector(".toana").value);
+  const nguvan = parseFloat(document.querySelector(".nguvana").value);
+  const tohop = parseFloat(document.querySelector(".tohopa").value);
+  const tb12 = parseFloat(document.querySelector(".tb12a").value);
+  const khuyenkhich = parseFloat(document.querySelector(".khuyenkhicha").value);
+  const uutien = parseFloat(document.querySelector(".uutiena").value);
   const notif = document.querySelector(".notif");
   const show = document.querySelector(".show_pointa");
   const sum = (((((toan+nguvan+tohop) / 3) + (khuyenkhich/4)) * 7 + (tb12 *3)) / 10) + uutien
